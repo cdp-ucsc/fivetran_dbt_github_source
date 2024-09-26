847a2c22a555ad8bfd48f4e7bca7e4a9cb94926c
Todd Graham <tgraham@ucsc.edu>
2024-09-26 14:36:17 -0700

    adding milestone stg

---
+++ models/stg_github__milestone.sql
@@ -0,0 +1,46 @@
+with milestone as (
+
+    select *
+    from {{ ref('stg_github__milestone_tmp') }}
+
+), macro as (
+    select
+        /*
+        The below macro is used to generate the correct SQL for package staging models. It takes a list of columns 
+        that are expected/needed (staging_columns from dbt_github_source/models/tmp/) and compares it with columns 
+        in the source (source_columns from dbt_github_source/macros/).
+
+        For more information refer to our dbt_fivetran_utils documentation (https://github.com/fivetran/dbt_fivetran_utils.git).
+        */
+            {{
+            fivetran_utils.fill_staging_columns(
+                source_columns=adapter.get_columns_in_relation(ref('stg_github__issue_tmp')),
+                staging_columns=get_issue_columns()
+            )
+        }}
+
+    from milestone 
+
+), fields as (
+
+    select 
+        id as milestone_id,
+        cast(created_at as {{ dbt.type_timestamp() }}) as created_at,
+        cast(updated_at as {{ dbt.type_timestamp() }}) as updated_at,
+        number as milestone_number,
+        state,
+        title,
+        description,
+        is_deleted,
+        cast(closed_at as {{ dbt.type_timestamp() }}) as closed_at,
+        cast(due_on as {{ dbt.type_timestamp() }}) as due_on,
+        repository_id,
+        creator_id,
+        _fivetran_synced
+
+    from macros
+
+)
+
+select * 
+from fields
